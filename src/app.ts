4fd2df5b6787279c42435d8219a8e3bd62a13c39
Vasylkivskyi <pavel.vasylkivskiy@gmail.com>
2023-04-10 21:25:25 +0300

    add prisma and database connection

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,25 @@
 import express, { Request, Response } from 'express'
+import { PrismaClient } from '@prisma/client'
 
+const prisma = new PrismaClient()
 const PORT = process.env.port || 8080
 const app = express()
+app.use(express.json())
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello')
+})
+
+app.post('/question', async (req, res) => {
+  const { email, password, name } = req.body
+  const user = await prisma.user.create({
+    data: {
+      email,
+      password,
+      name,
+    },
+  })
+  res.json(user)
 })
 
 app.listen(PORT, () => {
